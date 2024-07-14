4cc066c55219af93d73bb626d6ded6b870522a56
Mark Carrington <frumarkcarrington@gmail.com>
2024-07-14 07:32:57 +0530

    installed socket initialized socket on the server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 require("dotenv").config({ path: "./.env" });
 const { createServer } = require("http");
-
+const { Server } = require("socket.io");
 const server = createServer();
+const socket = new Server(server, {});
+socket.on("connection", (socket) => {
+  console.log(socket);
+});
 
 server.listen(process.env.PORT, () =>
   console.log(`The server is running on ${PORT}`)
