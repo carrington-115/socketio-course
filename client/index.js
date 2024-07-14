8e5573d597bdc0fd5b9ed5c2fd8965dbedfc3852
Mark Carrington <frumarkcarrington@gmail.com>
2024-07-14 15:41:25 +0530

    learned how to communicate with client and server using sockets

--- client/index.js
+++ client/index.js
@@ -3,3 +3,9 @@ const socket = io("http://localhost:5000");
 socket.on("connect", (response) => {
   console.log(response);
 });
+
+socket.on("message", (data) => {
+  console.log(data);
+});
+
+socket.emit("message", "hi server");
--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 const server = createServer();
 const socket = new Server(server, {
-    cors: {
-        origin: 'http://127.0.0.1:5500'
-    }
+  cors: {
+    origin: "http://127.0.0.1:5500",
+  },
 });
 socket.on("connection", (socket) => {
-  console.log(socket);
+  // console.log(socket);
+  socket.on("message", (data) => console.log(data));
+  socket.emit("message", "hello world");
 });
 
 server.listen(process.env.PORT, () =>
